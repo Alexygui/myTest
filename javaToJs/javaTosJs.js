5686bb7421b182bcb57d28a31ec04bc84b6a75cb
alexygui <guigen.wang.cn@gmail.com>
2017-11-19 11:34:36 +0800

    1
    1

--- javaToJs/javaTosJs.js
+++ javaToJs/javaTosJs.js
@@ -1,85 +1,219 @@
 /**
- * Created by wangguigen on 2017/11/18.
+ * Created by gui on 2017/11/18.
  */
 $(function () {
-  //测试用
-  $('#javaBean').val($('#tpl').html());
+  $('#configTitle').text('123');
 
-  $('#beanBtn').click(function () {
-    //截取javaBean中可以用来生成json数据的有效数据
-    let javaBean = $('#javaBean').val().match(/public class .*? (public)/)[0];
-    console.log(javaBean);
+  $('#jsTree').click(function (e) {
+    let $t = $(e.target);
+    $('#configTitle').val(123);
+  });
 
+  $('#javaBean').change(function () {
+    //截取javaBean中可以用来生成json数据的有效数据，即从类的声明到所有的私有属性
+    //public class 类名 以后的字符串
+    let javaBean = $('#javaBean').val().match(/public class .*/gi)[0];
+    // console.log(javaBean);
+    // debugger;
+
+    //获取modelName；public class 类名
     let modelArr = javaBean.match(/public class \w+/)[0].split(' ');
     let modelName = 'Model' + modelArr[modelArr.length - 1];
-    console.log(modelName);
+    // console.log(modelName);
+    // debugger;
 
-    //用于拼接的model字符串
-    let modelJson ='var '+ modelName + 'Json = {' + modelName + ':{<br/>';
+
+    //拼接状态json
+    let xzStateStr = '//新增权限配置\n' +
+      'var XzState = {\n' +
+      modelName + ': {\n' +
+      'disable: []\n' +
+      '},\n';
+    // 'ModelCcx: {\n' +
+    // 'disable: []\n' +
+    // '}\n' +
+    // '}\n\n';
+    let ckStateStr = '//查看权限配置\n' +
+      'var CkState = {\n' +
+      modelName + ': {\n' +
+      'enable: []\n' +
+      '},\n';
+    // 'ModelCcx: {\n' +
+    // 'enable: []\n' +
+    // '}\n' +
+    // '}\n\n';
+    let chooseState = '//依据参数确定选择的状态配置\n' +
+      'var stateJson;\n' +
+      'if ("1" === sort && "true" === lookflg) {\n' +
+      'stateJson = XzState;\n' +
+      '} else {\n' +
+      'stateJson = CkState;\n' +
+      '}\n';
+
+
+    //用于拼接的model字符串modelJson
+    let modelJson = 'var ' + modelName + 'Json = {' + modelName + ':{\n';
 
 
     //将有效数据截取为json中的key及其注释在同一条字符串中，获取的值组成数组
-    let items = javaBean.match(/\/\*\*.*?\*\/.*?private \w+ \w+;/gi);
+    //(/** ... */ private ... ;)或者(// ... private ... ;)
+    let items = javaBean.match(/(\/\*\*.*?\*\/.*?private \w+ \w+;)|(\/\/.*?private \w+ \w+;)/gi);
+    // console.log(items);
+    // debugger;
 
     //遍历数组，获取key和注释放到即将拼接的数组中
-    let jsonArr = [];
-    for (let i = 0; i < items.length; i++) {
-      let json = [];
-      let item = items[i];
-      let comment = item.match(/[\w\u4e00-\u9fa5]+/)[0];
-      let tempArr = item.split(' ');
-      let temp = tempArr[tempArr.length - 1];
-      let key = temp.substring(0, temp.length - 1);
-      json[0] = key + ': {  //' + comment+'<br/>';
-      json[1] = '},<br/>';
-      jsonArr.push(json);
+    let mainArr = [];  //用于保存主model的配置的数组
+    if (null != items && items.length > 0) {
+      for (let i = 0; i < items.length; i++) {
+        let json = [];
+        let item = items[i];
+        let comment = item.match(/[\w\u4e00-\u9fa5]+/)[0];  //注释：多个中文或者字符
+        let tempArr = item.split(' ');
+        let temp = tempArr[tempArr.length - 1];
+        let key = temp.substring(0, temp.length - 1);  //键-key
+        $('#jsTree').append('<li><a href="javascript:void 0;" class="button" id="' + i + '">' + key + ':' + comment + '</a></li><br/>');
+        json[0] = key + ': {  //' + comment + '\n';
+        json[1] = '},\n';
+        if ('sfyx_st' === key) {
+          json.splice(1, 0, 'defaultValue: "VALID"\n');
+        }
+        mainArr.push(json);
+      }
     }
-    console.log(jsonArr);
-
-    for (let i = 0; i < jsonArr.length; i++) {
-      modelJson += jsonArr[i].join('');
-    }
-    modelJson = modelJson.substring(0, modelJson.length - 1);
-    modelJson += '},<br/>';
+    // console.log(mainArr);
 
 
-    //获取关联子对象的数组
-    let children = javaBean.match(/private List<\w+> \w+;/gi);
-    let childrenArr = [];
+    //获取关联从对象的数据
+    //private List<类名而非Map<String,Object>> 键名;
+    let children = javaBean.match(/private List<\w+> \w+;/g);
+    let childrenArr = [];  //用于保存从对象的数组
+    let modelChildren = '';  //拼接Model生成DetailModel和Collection的字符串
+    let relations = '';  //从model对主model的依赖关系
     //遍历数组，获取key和注释放到即将拼接的数组中
-    for (let i = 0; i < children.length; i++) {
-      let json = [];
-      let item = children[i];
-      let tempArr = item.split(' ');
-      let temp = tempArr[tempArr.length - 1];
-      let key = temp.substring(0, temp.length - 1);
-      json[0] = 'Model' + key + ': {';
-      json[1] = '},';
-      childrenArr.push(json);
+    if (children != null && children.length > 0) {
+      for (let i = 0; i < children.length; i++) {
+        let json = [];
+        let item = children[i];
+        let tempArr = item.split(' ');
+        let tempModelName = tempArr[1];
+        //从对象的类名
+        let beanName = tempModelName.substring(5, tempModelName.length - 1);
+        let thisModelName = 'Model' + beanName;
+        let temp = tempArr[2];
+        let key = temp.substring(0, temp.length - 1);
+        json[0] = thisModelName + ': {\n';
+        json[1] = '},\n';
+        childrenArr.push(json);
+
+        //拼接状态json
+        xzStateStr += thisModelName + ': {\n' +
+          'disable: []\n' +
+          '},\n';
+        ckStateStr += thisModelName + ': {\n' +
+          'enable: []\n' +
+          '},\n';
+
+        //拼接Model配置和依赖关系
+        // let thisname = childrenArr[j][0];
+        let configJson = modelName + 'Json';
+        modelChildren += '//创建从Model对象\n' +
+          'var ' + thisModelName + ' = DetailModel.extend({\n' +
+          'className: "' + thisModelName + '",\n' +
+          'initJson: ' + configJson + ',\n' +
+          'stateJson: stateJson,\n' +
+          'setModelName: function () {\n' +
+          'this.set("ModelName", "' + thisModelName + '" + (++modelIndex));\n' +
+          '}\n' +
+          '});\n\n' +
+          'var ' + beanName + 'Collection = Backbone.Collection.extend({\n' +
+          'model: ' + thisModelName + '\n' +
+          '});\n\n';
+
+        //添加依赖关系
+        relations += '{\n' +
+          'type: Backbone.HasMany,\n' +
+          'key: "' + key + '",\n' +
+          'relatedModel: ' + thisModelName + ',\n' +
+          'collectionType: ' + beanName + 'Collection\n' +
+          '},\n';
+      }
+
+      //拼接状态json
+      xzStateStr = xzStateStr.substring(0, xzStateStr.length - 2);
+      xzStateStr += '}\n\n';
+      ckStateStr = ckStateStr.substring(0, ckStateStr.length - 2);
+      ckStateStr += '}\n\n';
+
+      //删除末尾的',\n'，换行符算作一个字符
+      relations = relations.substring(0, relations.length - 2)
     }
 
-    for (let j = 0; j < childrenArr.length; j++) {
-      modelJson += childrenArr[j].join('');
-    }
-    modelJson = modelJson.substring(0, modelJson.length - 1);
-    modelJson += '};<br/>';
-    console.log(modelJson);
-    $('#json').html(modelJson);
+    //将修改状态的字符串拼接
+    let stateStr = xzStateStr + ckStateStr + chooseState;
 
-  }).trigger('click');  //测试用
+    //将定义属性的数组转换为字符串
+    //modelJson：总的配置json字符串
+    //mainArr：用于保存主model的配置的数组
+    //childrenArr：用于保存从对象的数组
+    //modelName：主Model的名字
+    //modelChildren：拼接Model生成DetailModel和Collection的字符串
+    //relations：从model对主model的依赖关系
+    //状态配置的js字符串
+    contactString(modelJson, mainArr, childrenArr, modelName, modelChildren, relations, stateStr);
 
+
+  });
+
+  //测试用
+  $('#javaBean').val($('#tpl').html()).trigger('change');
 });
 
-/*
+//将定义属性的数组转换为字符串
+//modelJson：总的配置json字符串
+//mainArr：用于保存主model的配置的数组
+//childrenArr：用于保存从对象的数组
+//modelName：主Model的名字
+//modelChildren：拼接Model生成DetailModel和Collection的字符串
+//relations：从model对主model的依赖关系
+function contactString(modelJson, mainArr, childrenArr, modelName, modelChildren, relations) {
+  let str = '';//拼接所有的字符串
 
- 获取所有的
- /\/\*\*.*?\*\/.*?private.*?;/gi
+  //先拼接状态控制的字符串
+  str += stateStr;
 
- /[\w\u4e00-\u9fa5]+/i
+  //将mainArr数组的子字符串先拼接，不需要逗号
+  for (let i = 0; i < mainArr.length; i++) {
+    modelJson += mainArr[i].join('');
+  }
+  // console.log(modelJson);
+  //删除末尾的',\n'，换行符算作一个字符
+  modelJson = modelJson.substring(0, modelJson.length - 2);
+  modelJson += '\n},\n';
 
- .match(/private \w+ \w+;/i)[0].split(' ')[2]
+  //将childrenArr数组的子字符串先拼接，不需要逗号
+  for (let j = 0; j < childrenArr.length; j++) {
+    modelJson += childrenArr[j].join('');
+  }
 
- /List<\w+> \w+;/gi
+  //删除末尾的',\n'，换行符算作一个字符
+  modelJson = modelJson.substring(0, modelJson.length - 2);
+  modelJson += '\n};\n\n';
 
- */
+  let relationToMain = '//创建主model对象\n' +
+    'var ' + modelName + ' = DetailModel.extend({\n' +
+    'className: "' + modelName + '",\n' +
+    'initJson: ' + modelName + 'Json,\n' +
+    'stateJson: stateJson,\n' +
+    'relations: [\n' +
+    relations + '\n' +
+    ']\n' +
+    '});\n\n';
+  // console.log(modelJson);
 
+  //将所有的字符串拼接
+  str += modelJson;
+  str += modelChildren;
+  str += relationToMain;
+
+  $('#json').val(str);
+}
--- javaToJs/test.js
+++ javaToJs/test.js
@@ -1,96 +1,136 @@
-var ModelHtbaJson = {
-  ModelHtba: {
-    id: { //ID
-    },
-    htqd_id: { //合同签订id
-    },
-    htlb_no: { //合同类别NO
-    },
-    fbgclb_no: { //分包类别
-    },
-    htbah: { //合同备案号
-    },
-    htmc: { //合同名称
-    },
-    xm_id: { //项目ID
-    },
-    xmmc: { //项目名称
-    },
-    jsgm: { //建设规模
-    },
-    xmdz: { //项目地址
-    },
-    bd_id: { //标段ID
-    },
-    bdmc: { //标段名称
-    },
-    jfdw_id: { //甲方单位ID
-    },
-    jfdwmc: { //甲方单位名称
-    },
-    xmfzr_id: { //项目负责人id
-    },
-    xmfzrxm: { //项目负责人名称
-    },
-    jfhtqdr: { //甲方合同签订人姓名
-    },
-    jfqdsj: { //甲方签订时间
-    },
-    yfdw_id: { //乙方单位ID
-    },
-    yfdwmc: { //乙方单位名称
-    },
-    xmjl_id: { //项目经理id
-    },
-    xmjlxm: { //项目经理名称
-    },
-    xmzj_id: { //项目总监id
-    },
-    xmzjxm: { //项目总监姓名
-    },
-    yfhtqdrmc: { //乙方合同签订人姓名
-    },
-    yfqdsj: { //乙方签订时间
-    },
-    htje: { //合同金额
-    },
-    htkgrq: { //合同开工日期
-    },
-    htjgrq: { //合同竣工日期
-    },
-    aqcsf: { //合同备案单位工程关联信息
-    },
-    acfyhzh: { //安措费银行账号
-    },
-    bazt_st: { //备案状态
-    },
-    fj_id: { //附件ID
-    },
-    cjr_id: { //创建人ID
-    },
-    cjsj: { //创建时间
-    },
-    xgr_id: { //修改人ID
-    },
-    xgsj: { //修改时间
-    },
-    sfyx_st: { //是否有效
-    },
-    sgzcbdwzz: { //施工总承包单位资质
-    },
-    acfzffs: { //安措费支付方式
-    },
-    acfyh: { //安措费银行
-    },
-    acfzh: { //安措费账号
-    },
-    nmgyh: { //农民工工资银行
-    },
-    nmgzh: { //农民工工资账号
-    },
-    lwfbnrnos: { //劳务分包内容
-    },
-    jldw: { //监理单位
-    },
-    ModeldwgcglList: {}
-  };
+var ModelHtbaJson = {ModelHtba:{
+  id: {  //ID
+  },
+  htqd_id: {  //合同签订id
+  },
+  htlb_no: {  //合同类别NO
+  },
+  fbgclb_no: {  //分包类别
+  },
+  htbah: {  //合同备案号
+  },
+  htmc: {  //合同名称
+  },
+  xm_id: {  //项目ID
+  },
+  xmmc: {  //项目名称
+  },
+  jsgm: {  //建设规模
+  },
+  xmdz: {  //项目地址
+  },
+  bd_id: {  //标段ID
+  },
+  bdmc: {  //标段名称
+  },
+  jfdw_id: {  //甲方单位ID
+  },
+  jfdwmc: {  //甲方单位名称
+  },
+  xmfzr_id: {  //项目负责人id
+  },
+  xmfzrxm: {  //项目负责人名称
+  },
+  jfhtqdr: {  //甲方合同签订人姓名
+  },
+  jfqdsj: {  //甲方签订时间
+  },
+  yfdw_id: {  //乙方单位ID
+  },
+  yfdwmc: {  //乙方单位名称
+  },
+  yfdwzz_no: {  //Column
+  },
+  xmjl_id: {  //项目经理id
+  },
+  xmjlxm: {  //项目经理名称
+  },
+  xmzj_id: {  //项目总监id
+  },
+  xmzjxm: {  //项目总监姓名
+  },
+  yfhtqdrmc: {  //乙方合同签订人姓名
+  },
+  yfqdsj: {  //乙方签订时间
+  },
+  htje: {  //合同金额
+  },
+  htkgrq: {  //合同开工日期
+  },
+  htjgrq: {  //合同竣工日期
+  },
+  aqcsf: {  //合同备案单位工程关联信息
+  },
+  acfyhzh: {  //安措费银行账号
+  },
+  bazt_st: {  //备案状态
+  },
+  fj_id: {  //附件ID
+  },
+  cjr_id: {  //创建人ID
+  },
+  cjsj: {  //创建时间
+  },
+  xgr_id: {  //修改人ID
+  },
+  xgsj: {  //修改时间
+  },
+  sfyx_st: {  //是否有效
+    defaultValue: "VALID"
+  },
+  sgzcbdwzz: {  //施工总承包单位资质
+  },
+  acfzffs: {  //安措费支付方式
+  },
+  acfyh: {  //安措费银行
+  },
+  acfzh: {  //安措费账号
+  },
+  nmgyh: {  //农民工工资银行
+  },
+  nmgzh: {  //农民工工资账号
+  },
+  lwfbnrnos: {  //劳务分包内容
+  },
+  jldw: {  //监理单位
+  },
+  sfqz: {  //是否签章
+  },
+  acfyh_no: {  //安措费银行NO
+  },
+  htdzpath: {  //合同签章文件
+  }
+},
+  ModelHtbadwgcgl: {
+  }
+};
+
+//创建从Model对象
+var ModelHtbadwgcgl = DetailModel.extend({
+  className: "ModelHtbadwgcgl",
+  initJson: ModelHtbaJson,
+  stateJson: stateJson,
+  setModelName: function () {
+    this.set("ModelName", "ModelHtbadwgcgl" + (++modelIndex));
+  }
+});
+
+var HtbadwgcglCollection = Backbone.Collection.extend({
+  model: ModelHtbadwgcgl
+});
+
+//创建主model对象
+var ModelHtba = DetailModel.extend({
+  className: "ModelHtba",
+  initJson: ModelHtbaJson,
+  stateJson: stateJson,
+  relations: [
+    {
+      type: Backbone.HasMany,
+      key: "dwgcglList",
+      relatedModel: ModelHtbadwgcgl,
+      collectionType: HtbadwgcglCollection
+    }
+  ]
+});
+
