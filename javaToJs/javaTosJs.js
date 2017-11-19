f8722613ec3f6a27aa454f1cc23644bcef688abb
alexygui <guigen.wang.cn@gmail.com>
2017-11-19 13:53:38 +0800

    添加js文件的头部和尾部
    添加js文件的头部和尾部

--- javaToJs/javaTosJs.js
+++ javaToJs/javaTosJs.js
@@ -12,21 +12,24 @@ $(function () {
   $('#javaBean').change(function () {
     //截取javaBean中可以用来生成json数据的有效数据，即从类的声明到所有的私有属性
     //public class 类名 以后的字符串
-    let javaBean = $('#javaBean').val().match(/public class .*/gi)[0];
+    let javaBean = $('#javaBean').val().match(/import.*? public class .*/gi)[0];
     // console.log(javaBean);
     // debugger;
 
-    //获取modelName；public class 类名
-    let modelArr = javaBean.match(/public class \w+/)[0].split(' ');
-    let modelName = 'Model' + modelArr[modelArr.length - 1];
-    // console.log(modelName);
+    //获取className；public class 类名
+    let classStr = javaBean.match(/\/\*\*.*?\*\/.*?public class \w+/)[0];
+    let classArr = classStr.split(' ');
+    let classComment = classStr.match(/[\w\u4e00-\u9fa5]+/)[0];
+    let className = classArr[classArr.length - 1];
+    let ModelName = 'Model' + className;
+    // console.log(className);
     // debugger;
 
 
     //拼接状态json
     let xzStateStr = '//新增权限配置\n' +
-      'var XzState = {\n' +
-      modelName + ': {\n' +
+      'let XzState = {\n' +
+      ModelName + ': {\n' +
       'disable: []\n' +
       '},\n';
     // 'ModelCcx: {\n' +
@@ -34,8 +37,8 @@ $(function () {
     // '}\n' +
     // '}\n\n';
     let ckStateStr = '//查看权限配置\n' +
-      'var CkState = {\n' +
-      modelName + ': {\n' +
+      'let CkState = {\n' +
+      ModelName + ': {\n' +
       'enable: []\n' +
       '},\n';
     // 'ModelCcx: {\n' +
@@ -43,16 +46,17 @@ $(function () {
     // '}\n' +
     // '}\n\n';
     let chooseState = '//依据参数确定选择的状态配置\n' +
-      'var stateJson;\n' +
+      'let stateJson;\n' +
       'if ("1" === sort && "true" === lookflg) {\n' +
       'stateJson = XzState;\n' +
       '} else {\n' +
       'stateJson = CkState;\n' +
-      '}\n';
+      '}\n\n';
 
 
     //用于拼接的model字符串modelJson
-    let modelJson = 'var ' + modelName + 'Json = {' + modelName + ':{\n';
+    let modelJson = '//所有Model和字段的初始化Json配置\n' +
+      'let ' + ModelName + 'Json = {' + ModelName + ':{\n';
 
 
     //将有效数据截取为json中的key及其注释在同一条字符串中，获取的值组成数组
@@ -84,70 +88,78 @@ $(function () {
 
 
     //获取关联从对象的数据
-    //private List<类名而非Map<String,Object>> 键名;
-    let children = javaBean.match(/private List<\w+> \w+;/g);
+    //先筛选出所有的含private的注释加变量声明的字符串，再从其中筛选出包含List<类名>的变量声明
     let childrenArr = [];  //用于保存从对象的数组
     let modelChildren = '';  //拼接Model生成DetailModel和Collection的字符串
     let relations = '';  //从model对主model的依赖关系
-    //遍历数组，获取key和注释放到即将拼接的数组中
-    if (children != null && children.length > 0) {
-      for (let i = 0; i < children.length; i++) {
-        let json = [];
-        let item = children[i];
-        let tempArr = item.split(' ');
-        let tempModelName = tempArr[1];
-        //从对象的类名
-        let beanName = tempModelName.substring(5, tempModelName.length - 1);
-        let thisModelName = 'Model' + beanName;
-        let temp = tempArr[2];
-        let key = temp.substring(0, temp.length - 1);
-        json[0] = thisModelName + ': {\n';
-        json[1] = '},\n';
-        childrenArr.push(json);
 
-        //拼接状态json
-        xzStateStr += thisModelName + ': {\n' +
-          'disable: []\n' +
-          '},\n';
-        ckStateStr += thisModelName + ': {\n' +
-          'enable: []\n' +
-          '},\n';
+    let allPrivate = javaBean.match(/(\/\*\*.*?\*\/.*?private .*? \w+;)|(\/\/.*?private .*? \w+;)/gi);
+    if (null != allPrivate && allPrivate.length > 0) {
+      //遍历数组，获取key和注释放到即将拼接的数组中
+      for (let k = 0; k < allPrivate.length; k++) {
+        //private List<类名而非Map<String,Object>> 键名;
+        let children = allPrivate[k].match(/(\/\*\*.*?\*\/.*? private List<\w+> \w+;)|(\/\/.*? private List<\w+> \w+;)/g);
+        if (children != null && children.length > 0) {
+          // for (let i = 0; i < children.length; i++) {
+          let json = [];
+          let item = children[0];
+          // debugger;
+          let comment = item.match(/[\w\u4e00-\u9fa5]+/)[0];  //注释：多个中文或者字符
+          let tempArr = item.split(' ');
+          let tempModelName = tempArr[tempArr.length - 2];
+          //从对象的类名
+          let beanName = tempModelName.substring(5, tempModelName.length - 1);
+          let thisModelName = 'Model' + beanName;
+          let temp = tempArr[tempArr.length - 1];
+          let key = temp.substring(0, temp.length - 1);
+          json[0] = thisModelName + ': {  //' + comment + '\n';
+          json[1] = '},\n';
+          childrenArr.push(json);
 
-        //拼接Model配置和依赖关系
-        // let thisname = childrenArr[j][0];
-        let configJson = modelName + 'Json';
-        modelChildren += '//创建从Model对象\n' +
-          'var ' + thisModelName + ' = DetailModel.extend({\n' +
-          'className: "' + thisModelName + '",\n' +
-          'initJson: ' + configJson + ',\n' +
-          'stateJson: stateJson,\n' +
-          'setModelName: function () {\n' +
-          'this.set("ModelName", "' + thisModelName + '" + (++modelIndex));\n' +
-          '}\n' +
-          '});\n\n' +
-          'var ' + beanName + 'Collection = Backbone.Collection.extend({\n' +
-          'model: ' + thisModelName + '\n' +
-          '});\n\n';
+          //拼接状态json
+          xzStateStr += thisModelName + ': {\n' +
+            'disable: []\n' +
+            '},\n';
+          ckStateStr += thisModelName + ': {\n' +
+            'enable: []\n' +
+            '},\n';
 
-        //添加依赖关系
-        relations += '{\n' +
-          'type: Backbone.HasMany,\n' +
-          'key: "' + key + '",\n' +
-          'relatedModel: ' + thisModelName + ',\n' +
-          'collectionType: ' + beanName + 'Collection\n' +
-          '},\n';
+          //拼接Model配置和依赖关系
+          // let thisname = childrenArr[j][0];
+          let configJson = ModelName + 'Json';
+          modelChildren += '//创建从Model对象\n' +
+            'let ' + thisModelName + ' = DetailModel.extend({\n' +
+            'className: "' + thisModelName + '",\n' +
+            'initJson: ' + configJson + ',\n' +
+            'stateJson: stateJson,\n' +
+            'setModelName: function () {\n' +
+            'this.set("ModelName", "' + thisModelName + '" + (++modelIndex));\n' +
+            '}\n' +
+            '});\n\n' +
+            'let ' + beanName + 'Collection = Backbone.Collection.extend({\n' +
+            'model: ' + thisModelName + '\n' +
+            '});\n\n';
+
+          //添加依赖关系
+          relations += '{\n' +
+            'type: Backbone.HasMany,\n' +
+            'key: "' + key + '",\n' +
+            'relatedModel: ' + thisModelName + ',\n' +
+            'collectionType: ' + beanName + 'Collection\n' +
+            '},\n';
+          // }
+
+        }
       }
-
       //拼接状态json
       xzStateStr = xzStateStr.substring(0, xzStateStr.length - 2);
-      xzStateStr += '}\n\n';
+      xzStateStr += '\n};\n\n';
       ckStateStr = ckStateStr.substring(0, ckStateStr.length - 2);
-      ckStateStr += '}\n\n';
+      ckStateStr += '\n};\n\n';
 
       //删除末尾的',\n'，换行符算作一个字符
       relations = relations.substring(0, relations.length - 2)
     }
-
     //将修改状态的字符串拼接
     let stateStr = xzStateStr + ckStateStr + chooseState;
 
@@ -155,11 +167,11 @@ $(function () {
     //modelJson：总的配置json字符串
     //mainArr：用于保存主model的配置的数组
     //childrenArr：用于保存从对象的数组
-    //modelName：主Model的名字
+    //className：主Model的名字
     //modelChildren：拼接Model生成DetailModel和Collection的字符串
     //relations：从model对主model的依赖关系
-    //状态配置的js字符串
-    contactString(modelJson, mainArr, childrenArr, modelName, modelChildren, relations, stateStr);
+    //状态配置和控制的js字符串
+    contactString(modelJson, mainArr, childrenArr, className, classComment, modelChildren, relations, stateStr);
 
 
   });
@@ -172,14 +184,38 @@ $(function () {
 //modelJson：总的配置json字符串
 //mainArr：用于保存主model的配置的数组
 //childrenArr：用于保存从对象的数组
-//modelName：主Model的名字
+//className：主Model的名字
 //modelChildren：拼接Model生成DetailModel和Collection的字符串
 //relations：从model对主model的依赖关系
-function contactString(modelJson, mainArr, childrenArr, modelName, modelChildren, relations) {
-  let str = '';//拼接所有的字符串
+//状态配置和控制的js字符串
+function contactString(modelJson, mainArr, childrenArr, className, classComment, modelChildren, relations, stateStr) {
+  //拼接所有的字符串
+  let str = '';
 
-  //先拼接状态控制的字符串
-  str += stateStr;
+  //拼接js文件头
+  jsHead = '/**\n' +
+    '*' + classComment + '表单js文件\n' +
+    '*created by ruixin on ' + (new Date()).Format('yyyy-MM-dd') + '.\n' +
+    '*/\n\n' +
+    '//流程相关变量声明\n' +
+    'var sort = GetQueryString("sort");  //流程环节\n' +
+    'var rwid = GetQueryString("rwid");  //任务id\n' +
+    'var wid = GetQueryString("wid");    //工作流id\n' +
+    'var sid = GetQueryString("sid");    //环节页面关系实例id\n' +
+    'var lookflg = GetQueryString("lookflg"); //可编辑标志\n' +
+    'var id = GetQueryString("id"); //主键ID\n' +
+    'if (GetQueryString("WdataId")) {\n' +
+    'id = GetQueryString("WdataId"); //工作流data_id\n' +
+    '}\n' +
+    'var type = GetQueryString("type"); //操作类型\n\n' +
+    'var status = GetQueryString("Wstatus");  //流程状态\n' +
+    'var edit = GetQueryString("edit"); //页面是否可保存\n' +
+    'var lcid = GetQueryString("lcid"); //工作流流程id\n' +
+    'var path = GetQueryString("path");  //生成的word的文件名（非路径，路径在后端拼接添加）\n\n' +
+    '//' + classComment + '的DetailModel对象实例\n' +
+    'var model' + className + ' = {};\n\n' +
+    '$(function () {\n';
+
 
   //将mainArr数组的子字符串先拼接，不需要逗号
   for (let i = 0; i < mainArr.length; i++) {
@@ -199,10 +235,11 @@ function contactString(modelJson, mainArr, childrenArr, modelName, modelChildren
   modelJson = modelJson.substring(0, modelJson.length - 2);
   modelJson += '\n};\n\n';
 
+  let ModelName = 'Model' + className;
   let relationToMain = '//创建主model对象\n' +
-    'var ' + modelName + ' = DetailModel.extend({\n' +
-    'className: "' + modelName + '",\n' +
-    'initJson: ' + modelName + 'Json,\n' +
+    'let ' + ModelName + ' = DetailModel.extend({\n' +
+    'className: "' + ModelName + '",\n' +
+    'initJson: ' + ModelName + 'Json,\n' +
     'stateJson: stateJson,\n' +
     'relations: [\n' +
     relations + '\n' +
@@ -210,10 +247,88 @@ function contactString(modelJson, mainArr, childrenArr, modelName, modelChildren
     '});\n\n';
   // console.log(modelJson);
 
-  //将所有的字符串拼接
-  str += modelJson;
-  str += modelChildren;
-  str += relationToMain;
+
+  let modelName = 'model' + className;
+  //将所有的字符串拼接------------------------------
+  str += jsHead;  //拼接表头
+  str += stateStr;  //拼接状态控制的字符串
+  str += modelJson;  //拼接ModelJson字符串
+  str += modelChildren;  //拼接从Model对象的声明
+  str += relationToMain;  //拼接主Model对象
+  //拼接保存、删除方法
+  str += '});\n\n' +
+    '//保存验证\n' +
+    'function checkSheetSave() {\n' +
+    'var checkflg = false;\n' +
+    'checkflg = ' + modelName + '.saveVariable();\n' +
+    'return checkflg;\n' +
+    '}\n\n' +
+    '//保存方法\n' +
+    'function sheetSave() {\n' +
+    'var saveResult = {msg: "", flg: false};\n' +
+    '$.ajax({\n' +
+    'type: "post",\n' +
+    'url: "/ruixin/save' + className + '",\n' +
+    'data: { ' + modelName + ': ' + modelName + '.getJson() , wid: wid, sid: sid, sort: sort},\n' +
+    'async: false,\n' +
+    'dataType: "json",\n' +
+    'success: function (ar) {\n' +
+    'if (ar.success) {\n' +
+    'saveResult.flg = true;\n' +
+    '} else {\n' +
+    'saveResult.msg = ar.msg;\n' +
+    'saveResult.flg = false;\n' +
+    '}\n' +
+    '}\n' +
+    '});\n' +
+    'return saveResult;\n' +
+    '}\n\n' +
+    '// 删除验证\n' +
+    'function checkSheetDelete() {\n' +
+    '// 办理状态为未提交时（第一环节）返回true，可删除；否则返回false，不可删除\n' +
+    'return "1"=== sort;    \n' +
+    '}\n\n' +
+    '// 删除表单\n' +
+    'function sheetDelete() {\n' +
+    'var delResult = {msg: "", flg: false};\n' +
+    '$.ajax({\n' +
+    'type: "get",\n' +
+    'url: "/ruixin/delete' + className + '?id=" + id + "&random=" + Math.random(),\n' +
+    'async: false,\n' +
+    'success: function (ar) {\n' +
+    'if (ar.success) {\n' +
+    'delResult.flg = true;\n' +
+    '} else {\n' +
+    'delResult.msg = ar.msg;\n' +
+    'delResult.flg = false;\n' +
+    '}\n' +
+    '}\n' +
+    '});\n' +
+    'return delResult;\n' +
+    '}\n\n';
 
   $('#json').val(str);
 }
+
+// 对Date的扩展，将 Date 转化为指定格式的String
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+// 例子：
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
+Date.prototype.Format = function (fmt) { //author: meizz
+  let o = {
+    "M+": this.getMonth() + 1, //月份
+    "d+": this.getDate(), //日
+    "h+": this.getHours() % 12 == 0 ? 12 : this.getHours() % 12, //小时
+    "H+": this.getHours(), //小时
+    "m+": this.getMinutes(), //分
+    "s+": this.getSeconds(), //秒
+    "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+    "S": this.getMilliseconds() //毫秒
+  };
+  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (let k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
+};
--- javaToJs/test.js
+++ javaToJs/test.js
@@ -1,136 +1,274 @@
-var ModelHtbaJson = {ModelHtba:{
-  id: {  //ID
-  },
-  htqd_id: {  //合同签订id
-  },
-  htlb_no: {  //合同类别NO
-  },
-  fbgclb_no: {  //分包类别
-  },
-  htbah: {  //合同备案号
-  },
-  htmc: {  //合同名称
-  },
-  xm_id: {  //项目ID
-  },
-  xmmc: {  //项目名称
-  },
-  jsgm: {  //建设规模
-  },
-  xmdz: {  //项目地址
-  },
-  bd_id: {  //标段ID
-  },
-  bdmc: {  //标段名称
-  },
-  jfdw_id: {  //甲方单位ID
-  },
-  jfdwmc: {  //甲方单位名称
-  },
-  xmfzr_id: {  //项目负责人id
-  },
-  xmfzrxm: {  //项目负责人名称
-  },
-  jfhtqdr: {  //甲方合同签订人姓名
-  },
-  jfqdsj: {  //甲方签订时间
-  },
-  yfdw_id: {  //乙方单位ID
-  },
-  yfdwmc: {  //乙方单位名称
-  },
-  yfdwzz_no: {  //Column
-  },
-  xmjl_id: {  //项目经理id
-  },
-  xmjlxm: {  //项目经理名称
-  },
-  xmzj_id: {  //项目总监id
-  },
-  xmzjxm: {  //项目总监姓名
-  },
-  yfhtqdrmc: {  //乙方合同签订人姓名
-  },
-  yfqdsj: {  //乙方签订时间
-  },
-  htje: {  //合同金额
-  },
-  htkgrq: {  //合同开工日期
-  },
-  htjgrq: {  //合同竣工日期
-  },
-  aqcsf: {  //合同备案单位工程关联信息
-  },
-  acfyhzh: {  //安措费银行账号
-  },
-  bazt_st: {  //备案状态
-  },
-  fj_id: {  //附件ID
-  },
-  cjr_id: {  //创建人ID
-  },
-  cjsj: {  //创建时间
-  },
-  xgr_id: {  //修改人ID
-  },
-  xgsj: {  //修改时间
-  },
-  sfyx_st: {  //是否有效
-    defaultValue: "VALID"
-  },
-  sgzcbdwzz: {  //施工总承包单位资质
-  },
-  acfzffs: {  //安措费支付方式
-  },
-  acfyh: {  //安措费银行
-  },
-  acfzh: {  //安措费账号
-  },
-  nmgyh: {  //农民工工资银行
-  },
-  nmgzh: {  //农民工工资账号
-  },
-  lwfbnrnos: {  //劳务分包内容
-  },
-  jldw: {  //监理单位
-  },
-  sfqz: {  //是否签章
-  },
-  acfyh_no: {  //安措费银行NO
-  },
-  htdzpath: {  //合同签章文件
+/**
+ *合同备案信息表单js文件
+ *created by ruixin on 2017-11-19.
+ */
+
+//流程相关变量声明
+var sort = GetQueryString("sort");  //流程环节
+var rwid = GetQueryString("rwid");  //任务id
+var wid = GetQueryString("wid");    //工作流id
+var sid = GetQueryString("sid");    //环节页面关系实例id
+var lookflg = GetQueryString("lookflg"); //可编辑标志
+var id = GetQueryString("id"); //主键ID
+if (GetQueryString("WdataId")) {
+  id = GetQueryString("WdataId"); //工作流data_id
+}
+var type = GetQueryString("type"); //操作类型
+
+var status = GetQueryString("Wstatus");  //流程状态
+var edit = GetQueryString("edit"); //页面是否可保存
+var lcid = GetQueryString("lcid"); //工作流流程id
+var path = GetQueryString("path");  //生成的word的文件名（非路径，路径在后端拼接添加）
+//合同备案信息的DetailModel对象实例
+var modelHtba = {}
+
+$(function () {
+//新增权限配置
+  let XzState = {
+    ModelHtba: {
+      disable: []
+    },
+    ModelHtbadwgcgl: {
+      disable: []
+    },
+    ModelHtbadwgcgl22222: {
+      disable: []
+    }
+  };
+
+//查看权限配置
+  let CkState = {
+    ModelHtba: {
+      enable: []
+    },
+    ModelHtbadwgcgl: {
+      enable: []
+    },
+    ModelHtbadwgcgl22222: {
+      enable: []
+    }
+  };
+
+//依据参数确定选择的状态配置
+  let stateJson;
+  if ("1" === sort && "true" === lookflg) {
+    stateJson = XzState;
+  } else {
+    stateJson = CkState;
   }
-},
-  ModelHtbadwgcgl: {
-  }
-};
+
+//所有Model和字段的初始化Json配置
+  let ModelHtbaJson = {
+    ModelHtba: {
+      id: {  //合同备案信息
+      },
+      htqd_id: {  //合同签订id
+      },
+      htlb_no: {  //合同类别NO
+      },
+      fbgclb_no: {  //分包类别
+      },
+      htbah: {  //合同备案号
+      },
+      htmc: {  //合同名称
+      },
+      xm_id: {  //项目ID
+      },
+      xmmc: {  //项目名称
+      },
+      jsgm: {  //建设规模
+      },
+      xmdz: {  //项目地址
+      },
+      bd_id: {  //标段ID
+      },
+      bdmc: {  //标段名称
+      },
+      jfdw_id: {  //甲方单位ID
+      },
+      jfdwmc: {  //甲方单位名称
+      },
+      xmfzr_id: {  //项目负责人id
+      },
+      xmfzrxm: {  //项目负责人名称
+      },
+      jfhtqdr: {  //甲方合同签订人姓名
+      },
+      jfqdsj: {  //甲方签订时间
+      },
+      yfdw_id: {  //乙方单位ID
+      },
+      yfdwmc: {  //乙方单位名称
+      },
+      yfdwzz_no: {  //Column
+      },
+      xmjl_id: {  //项目经理id
+      },
+      xmjlxm: {  //项目经理名称
+      },
+      xmzj_id: {  //项目总监id
+      },
+      xmzjxm: {  //项目总监姓名
+      },
+      yfhtqdrmc: {  //乙方合同签订人姓名
+      },
+      yfqdsj: {  //乙方签订时间
+      },
+      htje: {  //合同金额
+      },
+      htkgrq: {  //合同开工日期
+      },
+      htjgrq: {  //合同竣工日期
+      },
+      aqcsf: {  //合同备案单位工程关联信息
+      },
+      acfyhzh: {  //安措费银行账号
+      },
+      bazt_st: {  //备案状态
+      },
+      fj_id: {  //附件ID
+      },
+      cjr_id: {  //创建人ID
+      },
+      cjsj: {  //创建时间
+      },
+      xgr_id: {  //修改人ID
+      },
+      xgsj: {  //修改时间
+      },
+      sfyx_st: {  //是否有效
+        defaultValue: "VALID"
+      },
+      sgzcbdwzz: {  //合同备案单位工程test22222
+      },
+      acfzffs: {  //安措费支付方式
+      },
+      acfyh: {  //安措费银行
+      },
+      acfzh: {  //安措费账号
+      },
+      nmgyh: {  //农民工工资银行
+      },
+      nmgzh: {  //农民工工资账号
+      },
+      lwfbnrnos: {  //劳务分包内容
+      },
+      jldw: {  //监理单位
+      },
+      sfqz: {  //是否签章
+      },
+      acfyh_no: {  //安措费银行NO
+      },
+      htdzpath: {  //合同签章文件
+      }
+    },
+    ModelHtbadwgcgl: {  //合同备案单位工程关联信息
+    },
+    ModelHtbadwgcgl22222: {  //合同备案单位工程test22222
+    }
+  };
 
 //创建从Model对象
-var ModelHtbadwgcgl = DetailModel.extend({
-  className: "ModelHtbadwgcgl",
-  initJson: ModelHtbaJson,
-  stateJson: stateJson,
-  setModelName: function () {
-    this.set("ModelName", "ModelHtbadwgcgl" + (++modelIndex));
-  }
-});
+  let ModelHtbadwgcgl = DetailModel.extend({
+    className: "ModelHtbadwgcgl",
+    initJson: ModelHtbaJson,
+    stateJson: stateJson,
+    setModelName: function () {
+      this.set("ModelName", "ModelHtbadwgcgl" + (++modelIndex));
+    }
+  });
 
-var HtbadwgcglCollection = Backbone.Collection.extend({
-  model: ModelHtbadwgcgl
-});
+  let HtbadwgcglCollection = Backbone.Collection.extend({
+    model: ModelHtbadwgcgl
+  });
+
+//创建从Model对象
+  let ModelHtbadwgcgl22222 = DetailModel.extend({
+    className: "ModelHtbadwgcgl22222",
+    initJson: ModelHtbaJson,
+    stateJson: stateJson,
+    setModelName: function () {
+      this.set("ModelName", "ModelHtbadwgcgl22222" + (++modelIndex));
+    }
+  });
+
+  let Htbadwgcgl22222Collection = Backbone.Collection.extend({
+    model: ModelHtbadwgcgl22222
+  });
 
 //创建主model对象
-var ModelHtba = DetailModel.extend({
-  className: "ModelHtba",
-  initJson: ModelHtbaJson,
-  stateJson: stateJson,
-  relations: [
-    {
-      type: Backbone.HasMany,
-      key: "dwgcglList",
-      relatedModel: ModelHtbadwgcgl,
-      collectionType: HtbadwgcglCollection
-    }
-  ]
+  let ModelHtba = DetailModel.extend({
+    className: "ModelHtba",
+    initJson: ModelHtbaJson,
+    stateJson: stateJson,
+    relations: [
+      {
+        type: Backbone.HasMany,
+        key: "dwgcglList",
+        relatedModel: ModelHtbadwgcgl,
+        collectionType: HtbadwgcglCollection
+      },
+      {
+        type: Backbone.HasMany,
+        key: "dwgcglList",
+        relatedModel: ModelHtbadwgcgl22222,
+        collectionType: Htbadwgcgl22222Collection
+      }
+    ]
+  });
+
 });
+
+//保存验证
+function checkSheetSave() {
+  var checkflg = false;
+  checkflg = modelHtba.saveVariable();
+  return checkflg;
+}
+
+//保存方法
+function sheetSave() {
+  var saveResult = {msg: "", flg: false};
+  $.ajax({
+    type: "post",
+    url: "/ruixin/saveHtba",
+    data: {modelHtba: modelHtba.getJson(), wid: wid, sid: sid, sort: sort},
+    async: false,
+    dataType: "json",
+    success: function (ar) {
+      if (ar.success) {
+        saveResult.flg = true;
+      } else {
+        saveResult.msg = ar.msg;
+        saveResult.flg = false;
+      }
+    }
+  });
+  return saveResult;
+}
+
+// 删除验证
+function checkSheetDelete() {
+// 办理状态为未提交时（第一环节）返回true，可删除；否则返回false，不可删除
+  return "1" === sort;
+}
+
+// 删除表单
+function sheetDelete() {
+  var delResult = {msg: "", flg: false};
+  $.ajax({
+    type: "get",
+    url: "/ruixin/deleteHtba?id=" + id + "&random=" + Math.random(),
+    async: false,
+    success: function (ar) {
+      if (ar.success) {
+        delResult.flg = true;
+      } else {
+        delResult.msg = ar.msg;
+        delResult.flg = false;
+      }
+    }
+  });
+  return delResult;
+}
 
